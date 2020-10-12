0dc2aedcc8e80217b4645d8e20c0c327bcb42fc3
onavi001 <onavi.001@gmail.com>
2020-10-12 17:27:53 -0500

    tabla user_domicilio

---
+++ database/migrations/2020_10_12_221422_user_domicilio.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class UserDomicilio extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('user_domicilio', function (Blueprint $table) {
+            $table->id();
+            $table->string('user_id');
+            $table->string('domicilio');
+            $table->string('numero_exterior');
+            $table->string('colonia');
+            $table->string('cp');
+            $table->string('ciudad');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('user_domicilio');
+    }
+}
